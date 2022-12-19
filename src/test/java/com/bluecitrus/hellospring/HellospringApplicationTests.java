85a19912beccb1f8bc293592d6ab317525ff23f3
minseoky <minseok184@gmail.com>
2022-12-19 21:48:14 +0900

    test

---
+++ src/test/java/com/bluecitrus/hellospring/HellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluecitrus.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
